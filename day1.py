8e3ba396324e507383042088d33f36d5848156da
Heather Jeffery <heatherjeffery@hotmail.co.uk>
2020-12-02 16:34:08 +0000

    Update to day1 answer with file reading in separate function

--- day1.py
+++ day1.py
@@ -2,33 +2,33 @@
 Task:
 Find the two numbers in a list that sum to 2020 and multiply them together
 """
-def sum_and_multiply_2(expenses):
-    expenses_list = []
-    with open(expenses, 'r') as infile:
+
+def file_to_list(filename):
+    data_list = []
+    with open(filename, 'r') as infile:
         for line in infile:
-            expenses_list.append(line.strip("\n"))
+            data_list.append(line.strip("\n"))
+    for counter, i in enumerate(data_list):
+        data_list[counter] = int(i)
+    return data_list
+
+def sum_and_multiply_2(expenses_list):
     for i in expenses_list:
-        if str(2020 - int(i)) in expenses_list:
-            multiplied_total = int(i) * int(2020 - int(i))
+        if 2020 - i in expenses_list:
+            multiplied_total = i * (2020 - i)
     return multiplied_total
 
-def sum_and_multiply_3(expenses):
-    expenses_list = []
-    with open(expenses, 'r') as infile:
-        for line in infile:
-            expenses_list.append(line.strip("\n"))
-    counter = 0
-    for i in expenses_list:
-        remainder = 2020 - int(i)
+def sum_and_multiply_3(expenses_list):
+    for counter, i in enumerate(expenses_list):
+        remainder = 2020 - i
         for j in expenses_list[counter:]:
             for k in expenses_list[counter:]:
-                if int(j) + int(k) == remainder:
-                    multiplied_total = int(i) * int(j) * int(k)
-        counter += 1
+                if j + k == remainder:
+                    multiplied_total = i * j * k
     return multiplied_total
 
-if __name__ == '__main__':
-    multiplied_total = sum_and_multiply_2("expenses_report.txt")
-    print(f'Answer to part a: {multiplied_total}')
-    multiplied_total_3 = sum_and_multiply_3("expenses_report.txt")
-    print(f'Answer to part b: {multiplied_total_3}')
+data_list = file_to_list("expenses_report.txt")
+multiplied_total = sum_and_multiply_2(data_list)
+print(f'Answer to part a: {multiplied_total}')
+multiplied_total_3 = sum_and_multiply_3(data_list)
+print(f'Answer to part b: {multiplied_total_3}')
